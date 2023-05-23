6fdffb96c073f9d19206297ad9ef2e028ec85bb3
kolitoo <alex2107@outlook.fr>
2023-05-23 19:00:37 +0200

    opti mac

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: abourdon <abourdon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 18:58:40 by abourdon          #+#    #+#             */
-/*   Updated: 2023/05/23 16:14:51 by abourdon         ###   ########.fr       */
+/*   Updated: 2023/05/23 19:00:04 by abourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,8 +69,9 @@ void	ft_usleep(long int usec, t_philo *philo)
 {
 	long	time_action_start;
 
+	(void)philo;
 	time_action_start = ft_get_time();
-	usleep(usec * (800 + philo->arg->nbr_philo));
+	usleep(usec * 800);
 	while (ft_get_time() - time_action_start < usec)
 		usleep(usec / 10);
 }
